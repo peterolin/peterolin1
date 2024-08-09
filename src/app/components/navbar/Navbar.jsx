0f7069b933b7fd76ff05d37106a48396068ae74f
Peter Olin <peter.olin@gmail.com>
2024-08-09 06:32:39 +0100

    Baseline before accessing the database.

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-"use client"; 
+"use client";
 import Link from 'next/link'
 import React from 'react';
 import styles from "./navbar.module.css";
 
 const links = [
-    
+
     {
         id: 2,
         title: "Portfolio",
@@ -17,11 +17,11 @@ const links = [
         id: 4,
         title: "About",
         url: "/about"
-    },{
+    }, {
         id: 5,
         title: "Contact",
         url: "/contact"
-    },{
+    }, {
         id: 6,
         title: "Dashboard",
         url: "/dashboard"
@@ -31,21 +31,19 @@ const links = [
 
 const Navbar = () => {
     return (
-        <div >
             <div className={styles.container} >
                 <Link className={styles.logo} href="/">PETEROLIN</Link>
                 <div className={styles.links}>
-                {links.map((link) => (
-                    <Link key={link.id} href={link.url} className={styles.logo}>{link.title}</Link>
-                ))}
+                    {links.map((link) => (
+                        <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
+                    ))}
                 </div>
-            </div >
-            <button className={styles.logout} onClick={() => {
+                <button className={styles.logout} onClick={() => {
                 console.log("Logged out")
             }}>
                 Logout
             </button>
-        </div >
+            </div >
     )
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className="container">
+      <div >
         {/* Layout UI */}
-        <section><Navbar />
+        <section className="top_level_container"><Navbar />
           {children}
           <Footer /></section>
           </div>
